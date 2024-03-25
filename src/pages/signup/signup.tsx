37a823560fd2fe088e56efa8ec524ae4bd3e38b8
Shahzaib <shahzaib@deriv.com>
2024-03-25 17:17:33 +0800

    feat: react router v6 & pages integration

---
+++ src/pages/signup/signup.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export const Signup = () => {
+    return (
+        <>
+            <h1>Signup</h1>
+            <Link to='/'>Go to Homepage</Link>
+        </>
+    );
+};
