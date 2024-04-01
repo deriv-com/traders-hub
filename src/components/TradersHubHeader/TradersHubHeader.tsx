b3d73fbca04cacd9c7f001cbac3fd3050c61dd08
Thisyahlen <thisyahlen@regentmarkets.com>
2024-04-01 11:06:58 +0800

    chore: remove checkmark

--- src/components/TradersHubHeader/TradersHubHeader.tsx
+++ src/components/TradersHubHeader/TradersHubHeader.tsx
@@ -2,8 +2,6 @@ import { Text } from '@deriv-com/ui';
 
 import { DemoRealSwitcher } from '@/components';
 
-import Checkmark from '../../assets/svgs/checkmark.svg?react';
-
 export const TradersHubHeader = () => {
     return (
         <div className='flex flex-col gap-8 lg:flex-row w-1/2 lg:w-auto items-center'>
@@ -11,7 +9,6 @@ export const TradersHubHeader = () => {
                 Trader&apos;s Hub
             </Text>
             <DemoRealSwitcher />
-            <Checkmark />
         </div>
     );
 };
