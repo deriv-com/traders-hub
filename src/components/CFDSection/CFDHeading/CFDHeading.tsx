ff88deab3ac3c45ce3eccd67b3476115830ceb40
shontzu-deriv <shon.tzu@regentmarkets.com>
2024-04-24 17:19:52 +0800

    chore: remove hideHoverStyles

--- src/components/CFDSection/CFDHeading/CFDHeading.tsx
+++ src/components/CFDSection/CFDHeading/CFDHeading.tsx
@@ -18,7 +18,7 @@ const CompareAccountsButton = () => {
     if (!isAuthorized) return null;
 
     return (
-        <Button color='primary' onClick={() => navigate('/compare-accounts')} size='sm' variant='ghost' hideHoverStyles>
+        <Button color='primary' onClick={() => navigate('/compare-accounts')} size='sm' variant='ghost'>
             {title}
         </Button>
     );
